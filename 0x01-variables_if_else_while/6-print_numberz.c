f842e13a6ffb1fe7721b869b989df9c7a6839ed7
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2022-07-03 19:21:26 -0700

    6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - The starting point
+ * description: print the base 10 single digit
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+	}
+	printf("\n");
+	return (0);
+}
