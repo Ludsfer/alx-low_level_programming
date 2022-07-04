19a63822160d67dadb582a80aa9273e7ee949b1d
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2022-07-04 07:57:24 -0700

    9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - The starting point
+ * description: Write a program that prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
