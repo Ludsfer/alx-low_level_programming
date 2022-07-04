4a1b45e82c33d97196be4c2c2100b456ada713ff
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2022-07-04 07:59:44 -0700

    9-print_comb.c file correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - The starting point
- * description: Write a program that prints all possible combinations of single-digit numbers
+ * description: prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
