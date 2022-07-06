6495ffc3e098fd9aa438cd75a1a7c79f00dd517d
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2022-07-05 22:07:30 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - function that adds two integers and returns the result
+ * @a: first fun argument
+ * @b: second fun argument
+ * Return: returns the sum of the function
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
