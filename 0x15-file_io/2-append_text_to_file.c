5b05881b67fe840bdbb6183388663d22ecb2a155
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2022-08-28 14:36:34 +0000

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,14 +13,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 00600);
+	fd = open(filename, O_RDWR | O_APPEND, 00664);
 	if (fd == -1)
 		return (-1);
 
-	wr = write(fd, text_content, strlen(text_content));
-	if (wr == -1)
-		return (-1);
-
+	if (text_content != NULL)
+	{
+		wr = write(fd, text_content, strlen(text_content));
+		if (wr == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
