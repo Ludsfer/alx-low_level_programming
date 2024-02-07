0e00aca55cc15cef3561b2d4b720f9ac85148f66
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-02-07 17:20:54 +0000

    Completed

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search and print a value from an array of integer
+ * @array: The passed array of integers
+ * @size: The size of the array
+ * @value: The value searched for
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
